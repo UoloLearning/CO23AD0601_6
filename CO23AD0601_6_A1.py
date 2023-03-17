85242bc00cfbbde6eb285ffdd12fe2354e116cc3
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-03-17 11:05:04 +0530

    Create CO23AD0601_6_A1.py

---
+++ CO23AD0601_6_A1.py
@@ -0,0 +1,7 @@
+#Defining the variables
+name = "John"
+phone_number = 65847961
+#Printing the first line
+print("Name","-","Phn_Number")
+#Printing the details using variables
+print(name,"-",phone_number)
