abad2aa4b08c6d5ffe5548c51ba2b53bc620714b
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-03-17 11:14:53 +0530

    Create CO23AD0601_6_A2.py

---
+++ CO23AD0601_6_A2.py
@@ -0,0 +1,8 @@
+current_strength = 37
+new_students = 6
+total_strength = current_strength + new_students
+print('Total Strength: ',total_strength)
+
+total_marks = 1237
+average = total_marks / total_strength
+print("Avg. marks per student: ", average)
